940334bb7bd1ee4d4e798d10a0ec1e20d69bdd62
olv <zetamail1337@gmail.com>
2025-03-07 12:37:30 +0000

    feat: add rat

---
+++ src/animals/Rat.java
@@ -0,0 +1,16 @@
+/**
+ * @author Gem Griño
+ * @version 1.0
+ */
+
+public class Rat extends Animal {
+
+    public Rat() {
+        super(); 
+    }
+
+    @Override
+    public void render() {
+        System.out.print("_");
+    }
+}
